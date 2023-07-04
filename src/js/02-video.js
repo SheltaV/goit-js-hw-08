e18f46f91b22ac75726873bc1ca313f8736af60f
SheltaV <bluegiraffezaoz@gmail.com>
2023-07-04 12:38:59 +0300

    update-js-hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,20 @@ const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
 
-// player.on('timeupdate', onPlay);
 
-function onPlay() {
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(1))
-}
+player.on('timeupdate', onPlay);
 
-onPlay()
+function onPlay({seconds}) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds))
+};
+
+const timing = JSON.parse(localStorage.getItem('videoplayer-current-time'))
+
+player.setCurrentTime(timing).then(function(seconds) {}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
